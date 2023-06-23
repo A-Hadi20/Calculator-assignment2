b2cc5130a294f6ba437c75dc035c83b8b9337fa1
A.Hadi20 <abdulhadi.executive@gmail.com>
2023-06-23 08:19:58 +0500

    calculator

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
+let buttons = document.getElementById('#all-button');
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
+        if (e.target.innerhtml == '=') {
             string = eval(string);
             document.querySelector('input').value = string;
         }
         else {
             console.log(e.target)
-            string = string + e.target.innerHTML;
+            string = string + e.target.innerhtml;
             document.querySelector('input').value = string;
         }
     })
