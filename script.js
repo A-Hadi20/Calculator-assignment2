80e874d4783e39dd5f364f2b936e5eb5ed8dd6fd
A.Hadi20 <abdulhadi.executive@gmail.com>
2023-06-25 22:41:11 +0500

    calculator

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-let string = "";
-let buttons = document.getElementById('#all-button');
-Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerhtml == '=') {
-            string = eval(string);
-            document.querySelector('input').value = string;
-        }
-        else {
-            console.log(e.target)
-            string = string + e.target.innerhtml;
-            document.querySelector('input').value = string;
-        }
-    })
-})
+let doSum = (event) => {
+    event.preventDefault();
+
+    let num1 = +document.querySelector("#num1").value;
+    let num2 = +document.querySelector("#num2").value;
+
+    let result = num1 + num2;
+
+    document.querySelector("#result").innerHTML = result;
+};
+
+document.querySelector("#add-btn").addEventListener('click', doSum);
+
+let doSubtraction = (event) => {
+    event.preventDefault();
+
+    let num1 = +document.querySelector("#num1").value;
+    let num2 = +document.querySelector("#num2").value;
+
+    let result = num1 - num2;
+
+    document.querySelector("#result").innerHTML = result;
+};
+
+document.querySelector("#sub-btn").addEventListener('click', doSubtraction);
+
+let doMultiplication = (event) => {
+    event.preventDefault();
+
+    let num1 = +document.querySelector("#num1").value;
+    let num2 = +document.querySelector("#num2").value;
+
+    let result = num1 * num2;
+
+    document.querySelector("#result").innerHTML = result;
+};
+
+document.querySelector("#multi-btn").addEventListener('click', doMultiplication);
+
+let doDivision = (event) => {
+    event.preventDefault();
+
+    let num1 = +document.querySelector("#num1").value;
+    let num2 = +document.querySelector("#num2").value;
+
+    let result = num1 / num2;
+
+    document.querySelector("#result").innerHTML = result;
+};
+
+document.querySelector("#div-btn").addEventListener('click', doDivision);
+
+document.querySelector("#clear-btn").addEventListener('click', () => {
+    document.querySelector("#num1").value = "";
+    document.querySelector("#num2").value = "";
+    document.querySelector("#result").innerHTML = "";
+});
